4b1af65c55acbefc7e24ae714bde865b7138e0a2
rgilangn <ghz.527.re@gmail.com>
2019-04-21 09:40:00 +0700

    Add Flights Admin
    tambah file buat manajemen penerbangan, view(index), tambah(add),edit(chgFlight(

---
+++ application/views/indexFlight.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+    <!-- MY CSS -->
+
+    <link rel="stylesheet" href="<?= base_url(); ?>/assets/css/style.css">
+
+    <title><?php echo $judul ?></title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href=<?= base_url(); ?>>Telkom University</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link" href="<?= base_url(); ?>">Home</a>
+                    <a class="nav-item nav-link" href="<?= base_url(); ?>mahasiswa">Mahasiswa</a>
+                    <a class="nav-item nav-link" href="#">About</a>
+
+                </div>
+            </div>
+        </div>
+    </nav> 
+<div class="container">
+    <?php if ($this->session->flashdata('flash')) : ?>
+    <div class="row mt-3">
+        <div class="col-md-6">
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                Data Mahasiswa <strong>berhasil</strong> <?= $this->session->flashdata('flash'); ?>.
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        </div>
+    </div>
+    <?php endif; ?>
+
+
+    <div class="row mt-3">
+        <div class="col md-6">
+            <form action="" method="post">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Cari data mahasiswa ... " name="keyword">
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" type="submit">Cari</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col">
+            <h3 class="text-center">Daftar Mahasiswa</h3>
+            <?php if (empty($mahasiswa)) : ?>
+            <div class="alert alert-danger" role="alert">
+                Data tidak ditemukan
+            </div>
+            <?php endif; ?>
+
+            <table class="table mt-5">
+                <thead>
+                    <tr>
+                        <th class="text-center" scope="col">NAMA</th>
+                        <th class="text-center" scope="col">EMAIL</th>
+                        <th class="text-center" scope="col">NIM</th>
+                        <th class="text-center" scope="col">JURUSAN</th>
+                        <th class="text-center" scope="col">AKSI</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr><?php foreach ($mahasiswa as $mhs) : ?>
+                        <td class="text-center"><?= $mhs['nama']; ?></td>
+                        <td class="text-center"><?= $mhs['nim']; ?></td>
+                        <td class="text-center"><?= $mhs['email']; ?></td>
+                        <td class="text-center"><?= $mhs['jurusan']; ?></td>
+                        <td class="text-center">
+                            <a href="<?= base_url(); ?>mahasiswa/hapus/<?= $mhs['id'] ?>" class="badge badge-danger float-center" onclick="return confirm('Apakah anda yakin menghapus data ini?');" ?>hapus</a>
+                            <a href="<?= base_url(); ?>mahasiswa/ubah/<?= $mhs['id'] ?>" class="badge badge-success float-center" ?>ubah</a>
+                        </td>
+                    </tr>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
+            <div class="row mt-3">
+                <div class="col md-6 text-center mt-5">
+                    <a href="<?= base_url(); ?>mahasiswa/tambah " class="btn btn-primary">Tambah Data Mahasiswa</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div> 
