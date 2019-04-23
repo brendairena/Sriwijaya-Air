43d795316dc0fcb2ce704f37113e1b55e1d89028
rgilangn <ghz.527.re@gmail.com>
2019-04-23 20:08:09 +0700

    edit

--- application/controllers/C_Main_Page.php
+++ application/controllers/C_Main_Page.php
@@ -39,22 +39,30 @@ class C_Main_Page extends CI_Controller {
 		$this->load->view('checkin');
 	}
 
-	public function jadwal_penerbangan_user()
+	public function jadwal_penerbangan()
 	{
-		$this->load->view('header');
-		$this->load->view('jadwal_penerbangan_cust');
+		$this->load->view('jadwal_penerbangan');
 	}
 	public function indexFlight()
 	{
 		$this->load->view('indexFlight');
 	}
 
+	public function addFlight()
+	{
+		$this->load->view('tambah_penerbangan');
+	}
+	
 	public function hotelUser()
 	{
 		$this->load->view('header');
-		$this->load->view('daftar_hotel_cust');
+		$this->load->view('daftar_hotel');
 	}
 
+	public function addHotel()
+	{
+		$this->load->view('tambah_hotel');
+	}
 
 	// ADMIN
 
@@ -65,25 +73,9 @@ class C_Main_Page extends CI_Controller {
 
 	public function hotelAdmin()
 	{
-		$this->load->view('header');
+		
 		$this->load->view('daftar_hotel_admin');
 	}
 
-	public function addHotel()
-	{
-		$this->load->view('header');
-		$this->load->view('tambah_hotel_admin');
-	}
 
-	public function jadwal_penerbangan_admin()
-	{
-		$this->load->view('header');
-		$this->load->view('jadwal_penerbangan_admin');
-	}
-
-	public function addFlight()
-	{
-		$this->load->view('header');
-		$this->load->view('tambah_penerbangan_admin');
-	}
 }
--- application/models/FlightModel.php
+++ application/models/FlightModel.php
@@ -1,20 +1,32 @@
 <?php
 
 class FlightModel extends CI_Model{
+
     public function getFlight(){
+        $this->db->select(*);
+        //$this->db->from();
+        //$que = $this->db->get();
+        return $que->result();
+    }
+
+        public function addFlight(){
 
     }
 
-    public function addFlight(){
+        public function hapusFlight($){
 
     }
 
-    public function hapusFlight(){
-
-    }
-
-    public function editFlight(){
+        public function editFlight(){
         
     }
+        // HOTELS MODELS
+        public function getHotel(){
+
+    }
+        public function addFlight(){
+
+    }
+
 
 }
